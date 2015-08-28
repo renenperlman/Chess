6a39596a91ae78593275fc1941b005d60885c14c
renenperlman <renenp@gmail.com>
2015-08-28 21:08:19 +0300

    Basic files
    The basic files.

---
+++ DS.h
@@ -0,0 +1,28 @@
+﻿struct listNode{
+	void *data;
+	struct listNode *next;
+	size_t size;
+};
+
+struct linkedList{
+	struct listNode *first;
+};
+
+struct pos{
+	char x;
+	int y;
+};
+
+struct piece{
+	char type;
+	int color; // 0 - white, 1 - black
+	char moved; // 0 - hasn't moved yet, 1 - moved
+	struct pos position;
+	(void) genMove(struct piece*, struct linkedList*);
+};
+
+struct move{
+	struct piece *p;
+	struct pos dest;
+};
+
